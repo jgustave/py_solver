fe11fc772202aac5555e197d5887f29ee23cb92d
jgustave <jerdavis@>
2021-01-07 21:32:33 -0800

    rewriting. added seperate clicks response

--- src/gensql.py
+++ src/gensql.py
@@ -35,32 +35,6 @@ pvars = ['ad_spend','clicks']
 
 rvars = ['conversions','revenue']
 
-
-
-#TODO: clicks as rvars
-
-
-#
-# select customer_type,'none' as location,product_category,business_category,order_channel,"_2019-10-19","_2019-10-20" from
-# crosstab( 'select rank() OVER (ORDER BY product_category,business_category) as rn,
-# 		   product_category,business_category,date,c_orders
-# 		FROM
-# 		 (select b.category_type as product_category,b.category_nm as business_category,a.date,a.c_orders,a.c_revenue from
-# 			( select date(event_ts) as date, category_id, sum(orders) as c_orders, sum(revenue) as c_revenue from category_segment_actuals group by 1,2 ) a
-# 			join
-# 			category b on (a.category_id = b.id)) aaa
-# 		 ORDER BY 1'
-# 		 ,$$VALUES ('2019-10-19'),('2019-10-20')$$ )
-# 		as bfoo
-#             (rn int,
-#              product_category text,business_category text,
-#              "_2019-10-19" float,"_2019-10-20" float
-#             )
-# cross join (select distinct order_channel from category_segment_actuals) c
-# cross join (values ('new'),('existing')) bbb(customer_type)
-
-
-
 for rvar in rvars:
 
     basic = '''copy (select customer_type,'none' as location,product_category,business_category,order_channel,''' + dstr0 + ''' 
@@ -82,21 +56,30 @@ for rvar in rvars:
 
     print(basic + b2 + '\n')
 
-# select f.platform_channel,e.tactic_nm,d.category_type as product_category, d.category_nm as business_category,a.event_ts,a.clicks,a.ad_spend
-# from campaign_performance a
-# join campaign b
-# on (a.campaign_id = b.id)
-# join campaign_category_mapping c
-# on (a.campaign_id=c.campaign_id)
-# join category d
-# on (c.category_id=d.id)
-# join tactic e
-# on (c.tactic_id=e.id)
-# join platform_channel f
-# on (b.platform_channel_id=f.id)
+
+#Clicks as Response.
+basic = '''copy (select customer_type,'none' as location,product_category,business_category,order_channel,''' + dstr0 + ''' 
+from crosstab( 'select rank() OVER (ORDER BY product_category,business_category) as rn,
+       product_category,business_category,date, clicks '''
+
+b2 = ''' FROM 
+        (select e.category_type as product_category,e.category_nm as business_category, date(a.event_ts) as date,a.clicks
+            from campaign_performance a
+            join campaign_category_mapping b
+            on (a.campaign_id = b.campaign_id )
+            join category e on (e.id=b.category_id) ) zzz
+     ORDER BY 1',''' + dstr1 + ''' as bfoo
+        (rn int,
+         product_category text,business_category text,
+         ''' + dstr2 + ''' )
+     cross join (select distinct order_channel from category_segment_actuals) c
+     cross join (values ('new'),('existing')) bbb(customer_type)
+    ) to '/tmp/clicks.csv' csv header;'''
+print(basic + b2 + '\n')
 
 
-#Keep all dimensions
+
+#predictors
 for pvar in pvars:
 
     basic = '''copy
