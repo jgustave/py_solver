0f420ef5f53205cbe1b4dadcec86cd3a75e08106
jgustave <jerdavis@>
2021-01-07 20:10:37 -0800

    rewriting.

--- src/gensql.py
+++ src/gensql.py
@@ -32,52 +32,68 @@ for d in dl:
 dstr2 = dstr2[0:-1]
 
 pvars = ['ad_spend','clicks']
-rvars = ['conversions','clicks','revenue']
+
+rvars = ['conversions','revenue']
 
 
-#drop first 2 dimensions
+
+#TODO: clicks as rvars
+
+
+#
+# select customer_type,'none' as location,product_category,business_category,order_channel,"_2019-10-19","_2019-10-20" from
+# crosstab( 'select rank() OVER (ORDER BY product_category,business_category) as rn,
+# 		   product_category,business_category,date,c_orders
+# 		FROM
+# 		 (select b.category_type as product_category,b.category_nm as business_category,a.date,a.c_orders,a.c_revenue from
+# 			( select date(event_ts) as date, category_id, sum(orders) as c_orders, sum(revenue) as c_revenue from category_segment_actuals group by 1,2 ) a
+# 			join
+# 			category b on (a.category_id = b.id)) aaa
+# 		 ORDER BY 1'
+# 		 ,$$VALUES ('2019-10-19'),('2019-10-20')$$ )
+# 		as bfoo
+#             (rn int,
+#              product_category text,business_category text,
+#              "_2019-10-19" float,"_2019-10-20" float
+#             )
+# cross join (select distinct order_channel from category_segment_actuals) c
+# cross join (values ('new'),('existing')) bbb(customer_type)
+
+
+
 for rvar in rvars:
 
-    basic = '''copy
-    (select customer_type,location,product_category, business_category,order_channel,''' + dstr0 + '''
-    from crosstab('select rank() OVER (ORDER BY marketing_channel, tactic, customer_type,location,product_category, business_category,order_channel) AS rn,
-                   marketing_channel, tactic, customer_type,location,product_category, business_category,order_channel, date, ''' + rvar
-    b2 = ''' from
-        (select platform_channel as marketing_channel,tactic_nm as tactic,customer_type,location,product_category,business_category,order_channel,event_ts as date,clicks,conversions,revenue,ad_spend 
-        from
-        (
-        select *, ''none'' as location from (
-        select f.platform_channel,e.tactic_nm,d.category_type as product_category, d.category_nm as business_category,a.event_ts,a.clicks,g.c_orders as conversions,g.c_revenue as revenue,a.ad_spend
-        from campaign_performance a
-        join campaign b
-        on (a.campaign_id = b.id)
-        join campaign_category_mapping c
-        on (a.campaign_id=c.campaign_id)
-        join category d
-        on (c.category_id=d.id)
-        join tactic e
-        on (c.tactic_id=e.id)
-        join platform_channel f
-        on (b.platform_channel_id=f.id)
-		left join (select date(event_ts) as c_date,category_id, sum(orders) as c_orders, sum(revenue) as c_revenue from category_segment_actuals group by 1,2) g
-	    on(c.category_id = g.category_id and a.event_ts = g.c_date)        
-        where category_nm != ''All''
-        order by b.platform_channel_id,tactic_nm,campaign_type,category_type,category_nm,event_ts
-        ) aaa
-        cross join (values (''new''),(''existing'')) bbb(customer_type)
-        cross join (select distinct order_channel from category_segment_actuals) ccc
-        ) ddd ) eee
-    order by 1 
-    ',''' + dstr1 + '''
-            as bfoo
+    basic = '''copy (select customer_type,'none' as location,product_category,business_category,order_channel,''' + dstr0 + ''' 
+    from crosstab( 'select rank() OVER (ORDER BY product_category,business_category) as rn,
+		   product_category,business_category,date, ''' + rvar
+
+    b2 = ''' FROM 
+		 (select b.category_type as product_category,b.category_nm as business_category,a.date,a.conversions,a.revenue from 
+			( select date(event_ts) as date, category_id, sum(orders) as conversions, sum(revenue) as revenue from category_segment_actuals group by 1,2 ) a
+			join
+			category b on (a.category_id = b.id)) aaa
+		 ORDER BY 1',''' + dstr1 + ''' as bfoo
             (rn int,
-             marketing_channel text, tactic text , customer_type text,location text,product_category text, business_category text,order_channel text,
-             ''' + dstr2 + '''
-            )
-    )
-    to '/tmp/''' + rvar + '''.csv' csv header;'''
+             product_category text,business_category text,
+             ''' + dstr2 + ''' )
+         cross join (select distinct order_channel from category_segment_actuals) c
+         cross join (values ('new'),('existing')) bbb(customer_type)
+        ) to '/tmp/''' + rvar + '''.csv' csv header;'''
 
     print(basic + b2 + '\n')
+
+# select f.platform_channel,e.tactic_nm,d.category_type as product_category, d.category_nm as business_category,a.event_ts,a.clicks,a.ad_spend
+# from campaign_performance a
+# join campaign b
+# on (a.campaign_id = b.id)
+# join campaign_category_mapping c
+# on (a.campaign_id=c.campaign_id)
+# join category d
+# on (c.category_id=d.id)
+# join tactic e
+# on (c.tactic_id=e.id)
+# join platform_channel f
+# on (b.platform_channel_id=f.id)
 
 
 #Keep all dimensions
@@ -88,11 +104,11 @@ for pvar in pvars:
     from crosstab('select rank() OVER (ORDER BY marketing_channel, tactic, customer_type,location,product_category, business_category,order_channel) AS rn,
                    marketing_channel, tactic, customer_type,location,product_category, business_category,order_channel, date, ''' + pvar
     b2 = ''' from
-        (select platform_channel as marketing_channel,tactic_nm as tactic,customer_type,location,product_category,business_category,order_channel,event_ts as date,clicks,conversions,revenue,ad_spend 
+        (select platform_channel as marketing_channel,tactic_nm as tactic,customer_type,location,product_category,business_category,order_channel,event_ts as date,clicks,ad_spend 
         from
         (
         select *, ''none'' as location from (
-        select f.platform_channel,e.tactic_nm,d.category_type as product_category, d.category_nm as business_category,a.event_ts,a.clicks,g.c_orders as conversions,g.c_revenue as revenue,a.ad_spend
+        select f.platform_channel,e.tactic_nm,d.category_type as product_category, d.category_nm as business_category,a.event_ts,a.clicks,a.ad_spend
         from campaign_performance a
         join campaign b
         on (a.campaign_id = b.id)
@@ -104,8 +120,6 @@ for pvar in pvars:
         on (c.tactic_id=e.id)
         join platform_channel f
         on (b.platform_channel_id=f.id)
-		left join (select date(event_ts) as c_date,category_id, sum(orders) as c_orders, sum(revenue) as c_revenue from category_segment_actuals group by 1,2) g
-	    on(c.category_id = g.category_id and a.event_ts = g.c_date)        
         where category_nm != ''All''
         order by b.platform_channel_id,tactic_nm,campaign_type,category_type,category_nm,event_ts
         ) aaa
@@ -127,3 +141,6 @@ for pvar in pvars:
 
     #existing,none,category,Charms,offline,
     #existing, none, category, Necklaces & Pendants, offline,
+
+    #marketing calendar.
+    #(3 values .. global, specific category, or none).
--- src/split.py
+++ src/split.py
@@ -22,13 +22,19 @@ for f in pvars :
         mc=a.iloc[0]['marketing_channel']
         #tactic=a.iloc[0]['tactic']
         #campaign=a.iloc[0]['campaign']
-        
+
         #Dropping tactic for now because it is duplicated in the data for some reason.. tactic contains business_category
         a.drop(columns=['marketing_channel','tactic'])\
             .sort_values(['customer_type','location','product_category','business_category','order_channel'])\
             .to_csv('/tmp/'+f+'/' + mc + '.csv', index=False)
 
 #Split only on marketing_channel.
+
+
+#(Demand Drivers)
+#FB and DV360
+#Marketing promotions ... (What if for all category?)
+
 
 
 
